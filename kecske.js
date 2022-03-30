59c0533876017bd84e80f099db4c23231457e1d9
Kalandor01 <rohovszkyakoska@gmail.com>
2022-03-30 11:40:14 +0200

    kész?

--- kecske.js
+++ kecske.js
@@ -8,13 +8,29 @@ function query_all(name, arg) {
     document.querySelectorAll(name).forEach(elem => {arg(elem)});
 }
 
-clicked_items = []
+var in_boat = []
 
 function click_img(evt)
 {
-    clicked_items.push(evt.target.src);
-    console.log(clicked_items);
+    in_boat.push(evt.target.src);
+    console.log(in_boat);
     evt.target.style.display = "none";
+    query("#csonak").innerHTML += `<img src="${evt.target.src}" alt="csonakban">`;
+    let farkas = false;
+    let kecske = false;
+    let kaposzta = false;
+    in_boat.forEach(animal => {
+        if((animal.split("/kepek/")[1]).indexOf("farkas") != -1)
+            farkas = true;
+        else if((animal.split("/kepek/")[1]).indexOf("kecske") != -1)
+            kecske = true;
+        else if((animal.split("/kepek/")[1]).indexOf("kaposzta") != -1)
+            kaposzta = true;
+    });
+    if(farkas && kecske)
+        alert("A farkas és a kecske nem utazhatnak együtt!");
+    if(kecske && kaposzta)
+        alert("A kecske és a káposzta nem utazhatnak együtt!");
 }
 
 function kiemel(evt)
@@ -30,7 +46,6 @@ function kiemel_out(evt)
 function init()
 {
     query("footer p").innerHTML = "Rohovszky Ákos";
-    query("article").innerHTML += '<img src="kepek/csonak.png" alt="csonak">';
     query_all("aside img", q=>q.onclick = click_img);
     query_all("aside img", q=>q.onmouseover = kiemel);
     query_all("aside img", q=>q.onmouseout = kiemel_out);
